bba7e07e5157d5f458b80f83dfa6adc1b97e4538
umeshdhakedCGI <umesh.dhaked@cgi.com>
2019-10-15 05:01:38 +0530

    .

--- src/main/java/com/stackroute/keepnote/config/WebAppInitializer.java
+++ src/main/java/com/stackroute/keepnote/config/WebAppInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 
 /*This class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
- * class in Servlet 3.0+ environments in order to configure the ServletContext 
+ * class in Servlet 3.0+ environments in order to configure the ServletContext
  * programmatically -- as opposed to the traditional web.xml-based approach. By overriding the methods of
  * class, we can define the Configuration classes and root mapping so that our application can gets
  * into spring.
@@ -14,22 +14,22 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-	
-		return new Class[] {WebMvcConfig.class};
+
+		return new Class[] {ApplicationContextConfig.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-	
+
 		return new Class[] {WebMvcConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		
+
 		return new String[] {"/"};
 	}
 
-	
+
 
 }
